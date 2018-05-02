6f46f2b93cf664cce616327a2e5038b3cac62ae8
AngryKitten <ctcrawford93@yahoo.com>
2018-05-02 16:00:03 -0400

    Drawing canvas initial creation
    * Updated .gitignore
    
    * Set up drawing canvas and clear with some placeholders.
    
    * Fixed gitignore and tracked useless file.

---
+++ app/src/main/java/com/example/android/learningaid/view/DrawingView.java
@@ -0,0 +1,65 @@
+package com.example.android.learningaid.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawingView extends View {
+    private final int paintColor = Color.BLACK;
+    private Paint drawPaint;
+    private Path path = new Path();
+
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        setupPaint();
+    }
+
+    private void setupPaint() {
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(5);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPath(path, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float pointX = event.getX();
+        float pointY = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(pointX, pointY);
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                path.lineTo(pointX, pointY);
+                break;
+            default:
+                return false;
+        }
+        postInvalidate();
+        return true;
+    }
+
+    public void clearCanvas() {
+        path.reset();
+        invalidate();
+    }
+
+    public void saveCanvas() {
+
+    }
+}
